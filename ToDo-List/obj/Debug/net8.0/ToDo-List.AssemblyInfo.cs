35ee0b77d1031a72720446ea5930e1a6dc9957b5
tsiachoque <tsiachoquet@ut.edu.co>
2024-10-09 23:26:46 -0500

    cambiando de rama

--- ToDo-List/obj/Debug/net8.0/ToDo-List.AssemblyInfo.cs
+++ ToDo-List/obj/Debug/net8.0/ToDo-List.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo-List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f415ac47f426e9c425be0bf41d18d1de2a644ec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada3a079abd4081a35c54de7a37605ee8e2aba5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo-List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo-List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
